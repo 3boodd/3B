0b50e047e3cd7e2d48fa075778c8b5432d2803fe
3boodd <36028899+3boodd@users.noreply.github.com>
2018-07-16 18:19:00 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -815,7 +815,6 @@ if (message.content.startsWith(prefix + 'setavatar')) {
 
 //////////////////////////////////////////////////////////////////////////////////
 
-const fs = require("fs"); // npm i fs
 const Canvas = require("canvas"); //npm i canvas
 const jimp = require("jimp"); //npm i jimp
 const moment = require("moment") //npm i moment
