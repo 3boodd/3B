988b54f54ef480c31996943f0d7add0db7e9605d
3boodd <36028899+3boodd@users.noreply.github.com>
2018-07-16 15:12:02 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -784,53 +784,34 @@ client.on('message', message => {
  })
 
 
-const KiNg66S = new Discord.Client();
 
-
-
-KiNg66S.on('message', message => {
-  if (!message.content.startsWith(prefix)) return;
+client.on('message', message => {
+  if (!message.content.startsWith(preifx)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== "430451101402267650") return;
+  if (message.author.id !== "332982196745011201") return;
 
-  
-  if (message.content.startsWith(prefix + 'setwatch')) {
-  KiNg66S.user.setActivity(argresult, {type: 'WATCHING'})
-     console.log('test' + argresult);
-    message.channel.sendMessage(`Watch Now: **${argresult}`)
-} 
-
- 
-  if (message.content.startsWith(prefix + 'setlis')) {
-  KiNg66S.user.setActivity(argresult, {type: 'LISTENING'})
-     console.log('test' + argresult);
-    message.channel.sendMessage(`LISTENING Now: **${argresult}`)
-} 
-
-
-if (message.content.startsWith(prefix + 'setname')) {
-  KiNg66S.user.setUsername(argresult).then
-      message.channel.sendMessage(`Username Changed To **${argresult}**`)
-  return message.reply("You Can change the username 2 times per hour");
-} 
-
-if (message.content.startsWith(prefix + 'setavatar')) {
-  KiNg66S.user.setAvatar(argresult);
-   message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
-}
-
-if (message.content.startsWith(prefix + 'setstream')) {
-  KiNg66S.user.setGame(argresult, "https://www.twitch.tv/KiNg66S");
-     console.log('test' + argresult);
-    message.channel.sendMessage(`Streaming: **${argresult}`)
-} 
-if (message.content.startsWith(prefix + 'setplay')) {
-  KiNg66S.user.setGame(argresult);
-     console.log('test' + argresult);
+if (message.content.startsWith(preifx + 'setgame')) {
+  client.user.setGame(argresult);
     message.channel.sendMessage(`Playing: **${argresult}`)
 } 
 
+if (message.content.startsWith(preifx + 'setstream')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/v5bz");
+	 console.log('test' + argresult);
+    message.channel.sendMessage(`Streaming: **${argresult}`)
+} 
+
+if (message.content.startsWith(preifx + 'setname')) {
+  client.user.setUsername(argresult).then
+	  message.channel.sendMessage(`Username Changed To **${argresult}**`)
+  return message.reply("You Can change the username 2 times per hour");
+} 
+if (message.content.startsWith(preifx + 'setavatar')) {
+  client.user.setAvatar(argresult);
+   message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
+}
+});
 
 
 });
