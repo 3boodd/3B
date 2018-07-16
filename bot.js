8f65ca67982e790dcd0e28387d0673244e5410ca
3boodd <36028899+3boodd@users.noreply.github.com>
2018-07-16 04:31:10 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -439,7 +439,7 @@ const fs = require('fs');
 let points = JSON.parse(fs.readFileSync('points.json', 'utf8'));
 client.on('message', message => {
     if (!points[message.author.id]) points[message.author.id] = {points : 0}
-    if (message.content == 'نقاطي'){
+    if (message.content == 'نقاطي13245632'){
         var embed = new RichEmbed()
         .setAuthor(message.author.username,message.author.avatarURL)
         .addField(`نقاطك : ${points[message.author.id].points}`,   true)
@@ -783,6 +783,59 @@ client.on('message', message => {
          }
  })
 
+
+  const Discord = require('discord.js');
+const PREFIX = "$"
+const KiNg66S = new Discord.Client();
+
+
+
+KiNg66S.on('message', message => {
+  if (!message.content.startsWith(PREFIX)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== "441683368418082836") return;
+
+  
+  if (message.content.startsWith(PREFIX + 'setwatch')) {
+  KiNg66S.user.setActivity(argresult, {type: 'WATCHING'})
+     console.log('test' + argresult);
+    message.channel.sendMessage(`Watch Now: **${argresult}`)
+} 
+
+ 
+  if (message.content.startsWith(PREFIX + 'setlis')) {
+  KiNg66S.user.setActivity(argresult, {type: 'LISTENING'})
+     console.log('test' + argresult);
+    message.channel.sendMessage(`LISTENING Now: **${argresult}`)
+} 
+
+
+if (message.content.startsWith(PREFIX + 'setname')) {
+  KiNg66S.user.setUsername(argresult).then
+      message.channel.sendMessage(`Username Changed To **${argresult}**`)
+  return message.reply("You Can change the username 2 times per hour");
+} 
+
+if (message.content.startsWith(PREFIX + 'setavatar')) {
+  KiNg66S.user.setAvatar(argresult);
+   message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
+}
+
+if (message.content.startsWith(PREFIX + 'setstream')) {
+  KiNg66S.user.setGame(argresult, "https://www.twitch.tv/KiNg66S");
+     console.log('test' + argresult);
+    message.channel.sendMessage(`Streaming: **${argresult}`)
+} 
+if (message.content.startsWith(PREFIX + 'setplay')) {
+  KiNg66S.user.setGame(argresult);
+     console.log('test' + argresult);
+    message.channel.sendMessage(`Playing: **${argresult}`)
+} 
+
+
+
+});
 
 
 client.login('NDUyMTUwMTk3NzE2Mzg1ODAz.DfMJQA.IdQc87KY6hdoOKwqDXRIfXo0h9w');
