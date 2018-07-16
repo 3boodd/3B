57b0bac329cb6758193e2fdda3b9be948fdb73e8
3boodd <36028899+3boodd@users.noreply.github.com>
2018-07-16 04:49:25 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -789,43 +789,43 @@ const KiNg66S = new Discord.Client();
 
 
 KiNg66S.on('message', message => {
-  if (!message.content.startsWith(PREFIX)) return;
+  if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
   if (message.author.id !== "430451101402267650") return;
 
   
-  if (message.content.startsWith(PREFIX + 'setwatch')) {
+  if (message.content.startsWith(prefix + 'setwatch')) {
   KiNg66S.user.setActivity(argresult, {type: 'WATCHING'})
      console.log('test' + argresult);
     message.channel.sendMessage(`Watch Now: **${argresult}`)
 } 
 
  
-  if (message.content.startsWith(PREFIX + 'setlis')) {
+  if (message.content.startsWith(prefix + 'setlis')) {
   KiNg66S.user.setActivity(argresult, {type: 'LISTENING'})
      console.log('test' + argresult);
     message.channel.sendMessage(`LISTENING Now: **${argresult}`)
 } 
 
 
-if (message.content.startsWith(PREFIX + 'setname')) {
+if (message.content.startsWith(prefix + 'setname')) {
   KiNg66S.user.setUsername(argresult).then
       message.channel.sendMessage(`Username Changed To **${argresult}**`)
   return message.reply("You Can change the username 2 times per hour");
 } 
 
-if (message.content.startsWith(PREFIX + 'setavatar')) {
+if (message.content.startsWith(prefix + 'setavatar')) {
   KiNg66S.user.setAvatar(argresult);
    message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
 }
 
-if (message.content.startsWith(PREFIX + 'setstream')) {
+if (message.content.startsWith(prefix + 'setstream')) {
   KiNg66S.user.setGame(argresult, "https://www.twitch.tv/KiNg66S");
      console.log('test' + argresult);
     message.channel.sendMessage(`Streaming: **${argresult}`)
 } 
-if (message.content.startsWith(PREFIX + 'setplay')) {
+if (message.content.startsWith(prefix + 'setplay')) {
   KiNg66S.user.setGame(argresult);
      console.log('test' + argresult);
     message.channel.sendMessage(`Playing: **${argresult}`)
