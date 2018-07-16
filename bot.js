6c5421c2d9856de4ab111468e7b44ea24b98f5d9
3boodd <36028899+3boodd@users.noreply.github.com>
2018-07-16 20:47:12 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1093,11 +1093,13 @@ message.channel.stopTyping()
 }
 });
 
+
+
+
 let sw = JSON.parse(fs.readFileSync("./setWlc.json", "UTF8"))
  
     client.on('message', message => {
 const Canvas = require("canvas") // npm i canvas
-const fs = require("fs") // npm i fs
  
         let mothed = ['text', 'embed', 'image'];
         let sets = message.content.split(" ").slice(1).join(" ")
